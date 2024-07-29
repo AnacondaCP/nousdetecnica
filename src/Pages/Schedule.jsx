8b4c8aa94c8f5f6e8559d7a3e8f3e31aad1abdf5
Bryanine <syawalhadi00718@gmail.com>
2024-07-29 21:51:18 +0700

    update lurr

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -21,11 +21,11 @@ const Schedule = () => {
 
     // Menentukan kelompok piket berdasarkan hari
     piketGroup = [
-        ["Amel", "Helmalia", "Fadli I", "Shera", "Nicollas"],
-        ["Windi", "Lista", "Jeriko", "Pratiwi", "Damar"],
-        ["Putri", "Paulista", "Firdaus", "Attala", "Ardian"],
-        ["Cariska", "Nila", "Wahid", "Togi", "Annisa"],
-        ["Sultan", "Zaini", "Fajri", "Arif"],
+        ["alex", "area", "abay", "apip", "damar g", "ramsyah", "syawal", "yeriko"], //senin
+        ["damar wi", "dani", "eriko", "fahri", "parel", "rehan", "yosua"], //selasa
+        ["pikri", "isnu", "kenji", "adli", "azhar", "putra", "wahyu"], //rabu
+        ["ryan", "nadhif", "nail", "tirsan", "ocean", "wisnu", "yudha"], //kamis
+        ["azka", "evelin", "gendis", "aini", "taskia", "nabil", "vincen"], //jumat
     ]
 
     const dayComponents = [
